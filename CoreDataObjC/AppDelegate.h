b6f09ecc7dbcf8fc10be257ba3ed9985abddd928
saipraveenkumar <sai_katteboina@simplicity-creations.com>
2017-08-23 14:38:12 +0530

    Initial Commit

---
+++ CoreDataObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataObjC
+//
+//  Created by Rushikesh Kulkarni on 23/08/17.
+//  Copyright © 2017 simplicity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
